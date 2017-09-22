dd6e4035400c87ff79fa22b664e7495f9fac0adb
Marisella.Restrepo <you@example.com>
2017-09-22 10:15:08 -0500

    Commit Taller2

--- src/AppMain.java
+++ src/AppMain.java
@@ -12,7 +12,7 @@ public class AppMain {
 		Empleado empleado2 = new Empleado(2,"Alejandro Moncada","1128400767",cargo2);
 		Empleado empleado3 = new Empleado(3,"Camilo Echeverri","1128400768",cargo2);
 		Empleado empleado4 = new Empleado(4,"Juan Pablo Moncada","1128400769",cargo);
-		Empleado empleado5 = new Empleado(5,"Sthefa Cardenas","1128400770",cargo2);
+		Empleado empleado5 = new Empleado(5,"Sthefa Cardenas","11284007701",cargo2);
 		Nomina nomina1 = new Nomina("30/09/2017");
 		nomina1.add(empleado1);
 		nomina1.add(empleado2);
